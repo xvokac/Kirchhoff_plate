c51a5bc7cff7a96d857cda55791b50b9dd2dbf74
xvokac <61604479+xvokac@users.noreply.github.com>
2026-02-18 17:51:02 +0100

    Update kirchhoff_plate.py

--- kirchhoff_plate.py
+++ kirchhoff_plate.py
@@ -661,7 +661,7 @@ def main():
     # Zobrazit síť co nejdříve před dalším výpočtem.
     fig_mesh = visualize_mesh(m, np.array([], dtype=int), basis, line_par_x, line_par_y)
     plt.show(block=False)
-    plt.pause(0.1)
+    plt.pause(2)
 
     # Včasné ověření, že linie grafů leží uvnitř oblasti.
     validate_query_line_in_polygon(line_par_x, axis="x", polygon=polygon)
